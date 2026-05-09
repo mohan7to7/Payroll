35b07cb5472f1ce503b9ce276fafae9770bb28d3
mohan7to7 <mohanakrishnakavali@Mohanas-MacBook-Air.local>
2026-05-08 21:20:09 -0400

    Exception handlers added

---
+++ src/main/java/com/dev/payroll/util/EmployeeNotFoundAdvice.java
@@ -0,0 +1,25 @@
+package com.dev.payroll.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class EmployeeNotFoundAdvice {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", HttpStatus.NOT_FOUND.value());
+        error.put("error", "Not Found");
+        error.put("message", ex.getMessage());
+        return error;
+    }
+}
